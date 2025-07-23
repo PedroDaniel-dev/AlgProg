2d5f2244be126d88b205310ef4ee664322afd682
pedro.bat <160679378+PedroDaniel-dev@users.noreply.github.com>
2025-07-22 23:59:59 -0300

    Update main.c
    adicionando as mudanças do hugo

--- main.c
+++ main.c
@@ -11,9 +11,9 @@
 
 struct cliente {
     char nome[TAM_NOME];
-    char dataNascimento[9]; // 22032005
-    char numTelefone[12];    // 21965759433
-    char cpf[12];            //21411681711
+    char dataNascimento[TAM_DATA]; // 22032005
+    char numTelefone[TAM_TEL];    // 21965759433
+    char cpf[TAM_CPF];            //21411681711
 };
 
 const char *nomesMeses[] = {"","Janeiro","Fevereiro","Março","Abril","Maio",
@@ -33,7 +33,7 @@ void cadastroClientes(struct cliente clientes[], int *quantidade) {
 }
 
 void exibirCliente(struct cliente c) {
-        //Saida de informaçoes
+    //Saida de informaçoes
     printf("Nome:%s\n", c.nome);
     printf("Nascimento:%s\n", c.dataNascimento);
     printf("Telefone:%s\n", c.numTelefone);
@@ -88,22 +88,12 @@ void buscarCPF(struct cliente clientes[], int quantidade) {
     }
 }
 
-/*void buscarCPF(struct cliente clientes[], int quantidade) {
-    printf("Digite o CPF a ser buscado.\n");
-    int cpfBuscado;
-    scanf("%d", &cpfBuscado);
-    for (int i = 0; i < MAX_CLIENTES; i++) {
-        if(cliente[i].cpf = cpfBuscado){
-            exibirCliente(i)
-        }
-}*/
-
 void buscarAniversariante(struct cliente clientes[], int quantidade, int mes){
     printf("Digite o mês para buscar aniversariantes:\n");
     int mesAniversario;
     scanf("%d", &mesAniversario);
     printf("Aniversários do mês %02d:\n", mes);
-    for (int i = 0; i < MAX_CLIENTES; i++) {
+    for (int i = 0; i < quantidade; i++) {
         // Comparar com os dígitos do mês na string "DD/MM/AAAA"
         if ((clientes[i].dataNascimento[3] - '0') * 10 + (clientes[i].dataNascimento[4] - '0') == mes) {
             printf("%s - %s\n", clientes[i].nome, clientes[i].dataNascimento);
@@ -115,31 +105,9 @@ int main(){
     struct cliente clientes[MAX_CLIENTES];
     int mesAniversario;
     int escolha;
-    int quantidaClientes = 0;
+    int quantidade = 0;
       printf("--------Bem-vindo!!!--------\n");
-    /*do {
-    printf("Bem-vindo!\nO que voce gostaria de fazer?\nSe quiser de fazer o cadastro digite (1)\nSe quiser Buscar por usuarios digite (2)\nSe quiser ver os aniversariantes do mes digite (3)\n");
-    scanf("%d", &escolha);
-    }
-    switch(escolha) {
-        case 1:
-            printf("--------Bem-vindo faça o cadastro!!!!--------\n");
-            while(quantidaClientes <= MAX_CLIENTES) {
-                printf("--------Faça o cadastro!!!!--------\n");
-                
-                cadastroClientes(clientes, &quantidaClientes);
-                exibirCliente(clientes[quantidaClientes - 1]);;
-                
-                quantidaClientes++;
-                break;
-                
-                
-        case 3:
-                printf("Em qual mes voce gostaria de consultar os aniversariantes?\n");
-                int mesAniversario;
-                scanf("%d", &mesAniversario);
-                buscarAniversariante(clientes, quantidaClientes, mesAniversario);
-    }*/
+    
     do {
     printf("\nO que você deseja fazer?\n");
     printf("1 - Cadastrar cliente\n");
@@ -150,22 +118,22 @@ int main(){
 
     switch (escolha) {
         case 1:
-            while (quantidaClientes < MAX_CLIENTES) {
+            while (quantidade < MAX_CLIENTES) {
                 printf("\n--------Cadastro de Cliente--------\n");
-                cadastroClientes(clientes, &quantidaClientes);
-                exibirCliente(clientes[quantidaClientes - 1]);
+                cadastroClientes(clientes, &quantidade);
+                exibirCliente(clientes[quantidade - 1]);
 
-                printf("\nDeseja cadastrar outro cliente? (1 - Sim / 0 - Não)\n");
+                /* printf("\nDeseja cadastrar outro cliente? (1 - Sim / 0 - Não)\n"); */
                 int opcao;
                 scanf("%d", &opcao);
                 if (opcao == 0) {
                     break;
-                }
+                } 
             }
             break;
             
-        case 2: buscarCPF(clientes, quantidaClientes); break;
-        case 3: buscarAniversariante(clientes, quantidaClientes, mesAniversario); break;
+        case 2: buscarCPF(clientes, quantidade); break;
+        case 3: buscarAniversariante(clientes, quantidade, mesAniversario); break;
         case 0: printf("Saindo...\n"); break;
         default: printf("Opção inválida!\n"); break;
     }
